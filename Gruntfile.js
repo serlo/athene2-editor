260fa87fd758d31b455fbbe7515f8d6f76c0cfc4
Ju <ju@formundcode.de>
2013-11-24 15:20:12 +0100

    welcome wiris no2

--- Gruntfile.js
+++ Gruntfile.js
@@ -300,7 +300,8 @@ module.exports = function (grunt) {
             server: {
                 options: {
                     port: 9001,
-                    base: '.'
+                    base: '.',
+                    open: true
                 }
             }
         }
---
+++ source/scripts/editor/plugins/wiris/wiris_plugin.js
@@ -0,0 +1,79 @@
+/*global define, require, MathJax*/
+define(['jquery', 'underscore', 'text!./editor/templates/plugins/wiris/wiris_plugin.html', 'texteditor_plugin', 'translator'], function ($, _, plugin_template, EditorPlugin, t) {
+    "use strict";
+    var FormulaPlugin,
+        wiris,
+        latex2mml = 'http://www.wiris.net/demo/editor/latex2mathml',
+        mml2latex = 'http://www.wiris.net/demo/editor/mathml2latex';
+
+    require(['http://www.wiris.net/demo/editor/editor'], function () {
+        wiris = com.wiris.jsEditor.JsEditor.newInstance({'language': 'en'});
+    });
+
+    function ajax (url, data) {
+        return $.ajax({
+            url: url,
+            method: 'get',
+            data: data
+        });
+    };
+
+    FormulaPlugin = function () {
+        this.state = 'math';
+        this.init();
+    };
+
+    FormulaPlugin.prototype = new EditorPlugin();
+    FormulaPlugin.prototype.constructor = FormulaPlugin;
+
+    FormulaPlugin.prototype.init = function () {
+        var self = this,
+            formular;
+
+        self.template = _.template(plugin_template);
+
+        self.data.name = 'Wiris';
+
+        // self.data.content = '\\lim_{n\\to\\infty} \\frac{n(n-1)(n-2)...(n-k)(n-k-1)...(1)}{(n-k)(n-k-1)...(1)}\\Big(\\frac{1}{n^k}\\Big)';
+
+        self.$el = $(self.template(self.data));
+        $('.content', self.$el).height(450);
+    };
+
+    FormulaPlugin.prototype.activate = function (token) {
+        var self = this;
+
+        formular = token.string;
+        self.data.content = formular.substr(2, formular.length - 4);
+
+        wiris.insertInto($('.content', self.$el)[0]);
+
+        ajax(latex2mml, "latex=" + encodeURIComponent(self.data.content))
+            .success(function (mml) {
+                wiris.setMathML(mml);
+            })
+            .fail(function () {
+                console.log(arguments);
+            });
+
+
+        self.$el.on('click', '.btn-success', function () {
+            self.save();
+        });
+    };
+
+    FormulaPlugin.prototype.save = function () {
+        var self = this,
+            data = wiris.getMathML();
+
+        ajax(mml2latex, "mml=" + encodeURIComponent(data))
+            .success(function (latex) {
+                self.data.content = latex;
+                self.trigger('save', self);
+            }).fail(function () {
+                console.log(arguments);
+            });
+    };
+
+    EditorPlugin.Wiris = FormulaPlugin;
+});
