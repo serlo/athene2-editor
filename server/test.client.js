4993903cef949991950bfbf5a02e5ee9a7660ea8
Ju <ju@formundcode.de>
2014-01-30 13:42:38 +0100

    added a formula and some inline code to the test string

--- server/test.client.js
+++ server/test.client.js
@@ -2,7 +2,7 @@ var dnode = require('dnode');
 
 var d = dnode.connect(7070);
 
-var json = '[[{"col":12,"content":"FE"},{"col":12,"content":"GAFE"}],[{"col":8,"content":"ADAW"},{"col":16,"content":"OK"}]]';
+var json = '[[{&quot;col&quot;:12,&quot;content&quot;:&quot;FE `inline code`&quot;},{&quot;col&quot;:12,&quot;content&quot;:&quot;GAFE\\n\\n$$\\frac12*a`*b`+12$$&quot;}],[{&quot;col&quot;:8,&quot;content&quot;:&quot;ADAW&quot;},{&quot;col&quot;:16,&quot;content&quot;:&quot;OK&quot;}]]';
 
 d.on('remote', function (remote) {
     remote.render(json, function (output, exception, message) {
