907a30ce7b56e92e3e296e446891afad015d3090
Ju <ju@formundcode.de>
2014-02-05 17:02:29 +0100

    build update

--- build/bower_components/requirejs/require.js
+++ build/bower_components/requirejs/require.js
@@ -1,6 +1,6 @@
 /**
  * 
- * Athene2 Editor - v0.1.3 
+ * Athene2 Editor - v0.1.4 
  *
  * @license LGPL-3.0
  * @license http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
