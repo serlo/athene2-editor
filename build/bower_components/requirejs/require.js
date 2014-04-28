e8a9f4bda9f7bad7ed79a8bbc2d37e33779811ad
Ju <ju@formundcode.de>
2014-04-28 19:10:36 +0200

    version bump 0.1.36

--- build/bower_components/requirejs/require.js
+++ build/bower_components/requirejs/require.js
@@ -1,6 +1,6 @@
 /**
  * 
- * Athene2 Editor - v0.1.35 
+ * Athene2 Editor - v0.1.36 
  *
  * @license LGPL-3.0
  * @license http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
